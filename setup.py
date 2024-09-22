d5e975f53ecc1057d740272b72f4174e7119746c
Eric Fu <me@ericfu.me>
2024-09-22 22:09:58 +0800

    update metadata

--- setup.py
+++ setup.py
@@ -1,20 +1,20 @@
-from setuptools import setup, find_packages
+from setuptools import setup
 
 # Read the contents of your README file
 with open("README.md", "r", encoding="utf-8") as f:
     long_description = f.read()
 
 setup(
-    name="loki-logger-handler",
-    version="0.0.2",
-    author="Xente",
-    description="Handler designed for transmitting logs to Grafana Loki in JSON format.",
+    name="loki-logging-handler",
+    version="0.0.3",
+    author="Eric Fu",
+    description="Logging handler to send logs to Grafana Loki",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/xente/loki-logger-handler",
-    packages=find_packages(),
+    url="https://github.com/fuyufjh/loki_logging_handler",
+    packages=["loki_logging_handler"],
     classifiers=[
-        "Development Status :: 5 - Production/Stable",
+        "Development Status :: 3 - Alpha",
         "Intended Audience :: Developers",
         "License :: OSI Approved :: MIT License",
         "Programming Language :: Python :: 3",
@@ -22,9 +22,12 @@ setup(
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+        "Programming Language :: Python :: 3.12",
     ],
-    keywords="loki, loguru, logger, handler",
-    install_requires=[],
+    keywords="loki, logging, handler",
+    install_requires=["requests==2.28.2"],
     test_suite="tests",
     license="MIT",
     python_requires=">=3.6",
